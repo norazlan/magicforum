c94cf3b0a79cd027690adbc5cb47d4978a75a247
norazlan <realsylo@gmail.com>
2016-08-04 23:43:52 +0800

    initial commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,54 @@
+class PostsController < ApplicationController
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render new_post_path
+    end
+  end
+
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+
+  def update
+    @post = Post.find_by(id: params[:id])
+
+    if @post.update(post_params)
+      redirect_to post_path(@post)
+    else
+      redirect_to edit_post_path(@post)
+    end
+  end
+
+  def destroy
+    @post = Post.find_by(id: params[:id])
+     if @post.destroy
+       redirect_to posts_path
+     else
+       redirect_to post_path(@post)
+     end
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  root to: 'landing#index'
+  get :about, to: 'static_pages#about'
+  resources :topics
+  resources :posts
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
