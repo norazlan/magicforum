5b75abf9106504df6990e58546b0adce5164b99f
norazlan <realsylo@gmail.com>
2016-08-09 17:21:16 +0800

    updated

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:success] = "Your account has been created"
+      redirect_to topics_path
+    else
+      flash[:danger] = @user.errors.full_messages
+      render new_user_path
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password_digest, :image)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
       resources :comments, except: [:show]
     end
   end
+  resources :users, only:[:new, :edit, :create, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
