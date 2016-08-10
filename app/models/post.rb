f1eaf5d53a73cc0613ea865d8ffc255ca02867d2
norazlan <realsylo@gmail.com>
2016-08-10 12:50:24 +0800

    updted

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   has_many :comments
   belongs_to :topic
+  belongs_to :user
   mount_uploader :image, ImageUploader
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,4 +2,5 @@ class Topic < ApplicationRecord
   has_many :posts
   validates :title, length: { minimum: 5 }, presence: true
   validates :description, length: { minimum: 20 }, presence: true
+  belongs_to :user
 end
