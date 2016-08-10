3b33cb2545045e22ada0bd79b8c9027e3aa9d202
norazlan <realsylo@gmail.com>
2016-08-10 12:23:52 +0800

    updated

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -27,6 +27,6 @@ class SessionsController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password)
+    params.permit(:email, :password)
   end
 end
