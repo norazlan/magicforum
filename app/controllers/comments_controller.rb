06cb15ef63cc3a25efd8bb6a96eb6b937596e6a5
norazlan <realsylo@gmail.com>
2016-08-11 17:22:42 +0800

    updated comment controller to manage nesting under related post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,50 +1,66 @@
 class CommentsController < ApplicationController
 
+before_action :authenticate!, only: [:create, :edit, :update, :new, :destroy]
+
   def index
-    @post = Post.includes(:comments).find_by(id: params[:post_id])
-    @comments = @post.comments.order("created_at DESC")
+    @topic = Topic.includes(:posts).find_by(id: params[:topic_id])
+    @post = Post.find_by(id: params[:post_id])
+    @comments = @post.comments.order(id: :DESC)
   end
 
   def new
+    @topic = Topic.find_by(id: params[:topic_id])
     @post = Post.find_by(id: params[:post_id])
     @comment = Comment.new
     authorize @comment
   end
 
   def create
+    @topic = Topic.find_by(id; params[:topic_id])
     @post = Post.find_by(id: params[:post_id])
-    @comment = Comment.new(comment_params.merge(post_id: params[:post_id]))
+    @comment = current_user.comments.build(comment_params.merge(post_id: params[:post_id]))
 
     if @comment.save
-      redirect_to topic_post_comments_path(@post)
+      flash[:success] = "You have created a comment"
+      redirect_to topic_post_comments_path(@topic, @post)
     else
-      render new_topic_post_comment_path(@post)
+      flash[:danger] = @comment.errors.full_messages
+      render new_topic_post_comment_path(@topic, @post)
     end
   end
 
   def edit
     authorize @comment
+    @topic = Topic.find_by(id: params[:topic_id])
+    @post = Post.find_by(id: params[:post_id])
     @comment = Comment.find_by(id: params[:id])
   end
 
   def update
+    @topic = Topic.find_by(id: params[:topic_id])
     @post = Post.find_by(id: params[:post_id])
+    @comment = Comment.find_by(id: params[:id])
 
     if @comment.update(comment_params)
-      redirect_to topic_post_comments_path(@comment)
+      flash[:success] = "Comment Edited"
+      redirect_to topic_post_comments_path(@topic, @post)
     else
-      redirect_to edit_topic_post_comment_path(@comment)
+      flash[:danger] = @comment.errors.full_messages
+      redirect_to edit_topic_post_comment_path(@topic, @post)
     end
   end
 
   def destroy
+    @topic = Topic.find_by(id: params[:topic_id])
+    @post = Post.find_by(id: params[:post_id])
+    @comment = Comment.find_by(id: params[:id])
     authorize @comment
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:body)
+    params.require(:comment).permit(:body, :image)
   end
 
 end
