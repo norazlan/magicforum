a63d798d18792e5d391b3bada48d2069f2966647
norazlan <realsylo@gmail.com>
2016-08-11 10:19:42 +0800

    updated

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
   end
   resources :users, only:[:new, :edit, :create, :update]
   resources :sessions, only:[:new, :create, :destroy]
-  resources :password_resets, only:[:new, :edit, :create]
+  resources :password_resets, only:[:new, :edit, :create, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
