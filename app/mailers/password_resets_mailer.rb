2aa0f8b8142aafcbc19015ac640039b07613ad3a
norazlan <realsylo@gmail.com>
2016-08-11 10:59:41 +0800

    updated

--- app/mailers/password_resets_mailer.rb
+++ app/mailers/password_resets_mailer.rb
@@ -2,7 +2,7 @@ class PasswordResetsMailer < ApplicationMailer
 
     def password_reset_mail(user)
       @user = user
-      @url = "http://23.92.53.87:3000/password_resets/#{@user.password_reset_token}/edit"
+      @url = "#{ENV.fetch('SERVER_URL')}/password_resets/#{@user.password_reset_token}/edit"
       mail(to: @user.email, subject: "Your password has been reset")
     end
   end
