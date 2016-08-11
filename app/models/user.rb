c84a2a59f5156012230f8d4a0e560a59b7e0b60e
norazlan <realsylo@gmail.com>
2016-08-11 11:16:47 +0800

    updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   has_many :topics
   has_many :posts
   has_many :comments
+
+  enum role: [:user, :moderator, :admin]
 end
--- db/migrate/20160811031125_add_default_to_user_role.rb
+++ db/migrate/20160811031125_add_default_to_user_role.rb
@@ -1,4 +1,5 @@
 class AddDefaultToUserRole < ActiveRecord::Migration[5.0]
   def change
+    change_column :users, :role, :integer, default: 0
   end
 end
